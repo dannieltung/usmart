55ccf4511c6c0a07049ded7a239a2b4852ae10a9
Danniel Tung <dwftung@gmail.com>
2020-12-29 22:47:54 -0300

    controller started

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  private
+
+  def categories_params
+  end
+
+end
---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,27 @@
+class CreditCardsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  private
+
+  def credit_cards_params
+  end
+end
