cb2f772ad195b9e8d5b1358f6ea65f14bc523c15
Danniel Tung <dwftung@gmail.com>
2020-12-31 10:35:45 -0300

    starting some views

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,8 +1,16 @@
 class BuyersController < ApplicationController
   def new
+    @buyer = Buyer.new
   end
 
   def create
+    @buyer = Buyer.new
+    @buyer.user = current_user
+    if @buyer.save
+      redirect_to root_path, notice: 'Buyer created!'
+    else
+      render :new
+    end
   end
 
   def edit
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,16 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new
+    @category.user = current_user
+    if @category.save
+      redirect_to root_path, notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
   def edit
