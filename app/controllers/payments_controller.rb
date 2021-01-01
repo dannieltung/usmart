fb07ae229933ccbbf073e3d403f843305226e2b5
Danniel Tung <dwftung@gmail.com>
2021-01-01 17:34:10 -0300

    refactored due date payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,10 +5,8 @@ class PaymentsController < ApplicationController
       @payment = Payment.new(payments_params)
       @payment.user = current_user
       @payment.partial = partial
-      @payment.due_date = (Date.today + partial.month).change(day: CreditCard.find(params[:payment][:credit_card_id]).due_day)
-      b = Date.parse(params[:payment][:date]) < Date.today.change(day: CreditCard.find(params[:payment][:credit_card_id]).best_day)
+      Date.parse(params[:payment][:date]) >= Date.today.change(day: CreditCard.find(params[:payment][:credit_card_id]).best_day) ? @payment.due_date = (Date.today + (partial + 1).month).change(day: CreditCard.find(params[:payment][:credit_card_id]).due_day) : @payment.due_date = (Date.today + partial.month).change(day: CreditCard.find(params[:payment][:credit_card_id]).due_day)
       # a = Date.today.change(day: CreditCard.find(params[:payment][:credit_card_id]).best_day)
-      raise
       @payment.save
       partial += 1
     end
