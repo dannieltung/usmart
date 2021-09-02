c747bd16b05c8457a2a2e3b28f3c52123cd3a436
Danniel Tung <dwftung@gmail.com>
2021-09-02 14:45:47 -0300

    frontending

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
     # # end
     @payment = Payment.new
     @categories = Category.where(user_id: current_user.id).where.not(name: 'Não Definido').sort_by { |event| event.name }
-    @credit_cards = CreditCard.where(user_id: current_user.id, status: true).sort_by { |event| event.name }
+    # @credit_cards = CreditCard.where(user_id: current_user.id, status: true).sort_by { |event| event.name }
     # @buyers = Buyer.where(user_id: current_user.id).sort_by { |event| event.name }
     @payments = Payment.where(user_id: current_user.id).sort_by { |event| event.due_date }
   end
